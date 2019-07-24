8684540767064498ce58ad13ddc2ce958ad252ac
Joao de assis <jonjon667@hotmail.com>
2019-07-24 11:37:32 -0300

    Iniciando projeto e scripts de movimentação do jogador

---
+++ Assets/Scripts/Camera Scripts/CameraScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow: MonoBehaviour
+{
+    private Transform target;
+
+    private bool followPlayer;
+
+    private float min_y = -2.6f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Player Scripts/PlayerScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript: MonoBehaviour
+{
+    private Rigidbody2D myBody;
+
+    public float moveSpeed = 2f;
+
+    public float normal_push = 10f;
+    public float extra_push = 13f;
+
+    private int push_count;
+
+    private bool player_Died;
+    private bool initial_Push;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.tag == "ExtraPush")
+        {
+            if (!initial_Push)
+            {
+                initial_Push = true;
+                myBody.velocity = new Vector2(myBody.velocity.x, 18f);
+
+                target.gameObject.SetActive(false);
+
+                return;
+            }
+        }
+    } 
+}
